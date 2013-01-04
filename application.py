4fee7581fbf36a72f43a4ccdc873835885beb265
Daniel Lathrop - News Applications Editor <dlathrop@kpandadb1.ahc.belotechnologies.com>
2013-01-04 15:40:34 -0600

    modified application.py to work with our Redhat/apache setup

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-import os
+import os, sys
 
 import django.core.handlers.wsgi
+
+sys.path.append('/opt/panda')
+
 
 # When serving under WSGI (rather than runserver) use deployed config
 os.environ["DJANGO_SETTINGS_MODULE"] = "config.deployed.settings"
